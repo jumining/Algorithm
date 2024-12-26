15884b2df22d95a9056ee0dee526e907cc508a3d
jumining <76610340+jumining@users.noreply.github.com>
2024-12-26 19:39:39 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.py
@@ -1,28 +1,29 @@
 vowels = "aeiou"
 consonants = "bcdfghjklmnpqrstvwxyz"
+accept = "eo"
 
 
-def contains_vowels():
-    return any(char in vowels for char in pwd)
+def contains_vowels(s):
+    return any(char in vowels for char in s)
 
 
-def has_three_consecutive():
-    for i in range(len(pwd) - 2):
-        if all(char in vowels for char in pwd[i:i + 3]) or all(char in consonants for char in pwd[i:i + 3]):
+def has_three_consecutive(s):
+    for i in range(len(s) - 2):
+        if all(char in vowels for char in s[i:i + 3]) or all(char in consonants for char in s[i:i + 3]):
             return True
     return False
 
 
-def has_double_letter():
-    for i in range(len(pwd) - 1):
-        if pwd[i] == pwd[i + 1] and pwd[i] != "e" and pwd[i] != "o":
+def has_double_letter(s):
+    for i in range(len(s) - 1):
+        if s[i] == s[i + 1] and s[i] not in accept:
             return True
     return False
 
 
 pwd = input()
 while pwd != "end":
-    if not contains_vowels() or has_three_consecutive() or has_double_letter():
+    if not contains_vowels(pwd) or has_three_consecutive(pwd) or has_double_letter(pwd):
         print(f"<{pwd}> is not acceptable.")
     else:
         print(f"<{pwd}> is acceptable.")
