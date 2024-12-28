7d3b6e932c82f8f8bfddc91450daa9079e3f9a60
jumining <76610340+jumining@users.noreply.github.com>
2024-12-28 13:52:36 +0900

    [Silver III] Title: 크로스 컨트리, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.py
+++ 백준/Silver/9017. 크로스 컨트리/크로스 컨트리.py
@@ -1,39 +1,35 @@
-t = int(input())
-for _ in range(t):
+T = int(input())
+for _ in range(T):
     n = int(input())
-    count = {}   # 팀 당 주자 수
-    temp = list(map(int, input().split()))
+    inputs = list(map(int, input().split()))
 
-    # 팀 별로 주자 수 세기
+    count = {}
     for i in range(n):
-        if temp[i] in count:
-            count[temp[i]] += 1
+        if inputs[i] in count:
+            count[inputs[i]] += 1
         else:
-            count[temp[i]] = 1
+            count[inputs[i]] = 1
 
-    # 제외되는 팀 구하기
-    dele = {}   # 제외되는 팀
+    dele = {}
     for k, v in count.items():
         if v < 6:
             dele[k] = 1
 
-    # 점수 계산
     team = {}
-    idx = 1    # 점수
+    idx = 1
     for i in range(n):
-        if temp[i] not in dele:
-            if temp[i] in team:
-                if team[temp[i]][0] < 4:
-                    team[temp[i]][0] += 1
-                    team[temp[i]][1] += idx
-                elif team[temp[i]][0] == 4:
-                    team[temp[i]][0] += 1
-                    team[temp[i]][2] = idx
+        team_id = inputs[i]
+        if team_id not in dele:
+            if team_id in team:
+                if team[team_id][0] < 4:
+                    team[team_id][0] += 1
+                    team[team_id][1] += idx
+                elif team[team_id][0] == 4:
+                    team[team_id][0] += 1
+                    team[team_id][2] = idx
             else:
-                team[temp[i]] = [1, idx, 0]   # [팀당 주자 수, 상위 4명 점수 합, 5번째 주자의 점수]
-
+                team[team_id] = [1, idx, 0] # 주자 수, 상위 4명 점수, 5번째 선수 점수
             idx += 1
 
-    # 순위 정렬
     team = sorted(team.items(), key=lambda x:(x[1][1], x[1][2]))
     print(team[0][0])
