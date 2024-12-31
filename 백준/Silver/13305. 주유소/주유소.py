d106b204fe6f88369e739ebb9116623d38a4f71f
jumining <76610340+jumining@users.noreply.github.com>
2024-12-31 23:09:07 +0900

    [Silver III] Title: 주유소, Time: 116 ms, Memory: 48604 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -3,11 +3,10 @@ l_roads = list(map(int, input().split()))
 c_liters = list(map(int, input().split()))
 
 result = 0
-for idx, cost in enumerate(c_liters):
-    if idx < n_city-1 and cost == min(c_liters[idx:-1]):
-        result += cost * sum(l_roads[idx:])
-        break
-    else:
-        result += cost * l_roads[idx]
+min_cost = c_liters[0]
+
+for idx in range(n_city - 1):
+    min_cost = min(min_cost, c_liters[idx])
+    result += min_cost * l_roads[idx]
 
 print(result)
