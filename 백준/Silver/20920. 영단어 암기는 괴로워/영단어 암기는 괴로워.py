3ff2e5762175ca1defb29ba0ea6b907e721ad8f5
jumining <76610340+jumining@users.noreply.github.com>
2025-01-08 21:55:13 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 392 ms, Memory: 61036 KB -BaekjoonHub

---
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -0,0 +1,16 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+words = {}
+
+for _ in range(N):
+  word = sys.stdin.readline().strip()
+  if word in words:
+    words[word] += 1
+  else:
+    if len(word) >= M:
+      words[word] = 1
+  
+words = sorted(words.items(), key=lambda x: (-x[1], -len(x[0]), x[0]))
+for w, _ in words:
+  print(w)
