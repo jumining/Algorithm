074a4815f932828cd7865f0f58f22a4c053803ae
jumining <76610340+jumining@users.noreply.github.com>
2025-02-26 18:57:53 +0900

    [Silver IV] Title: 카드2, Time: 204 ms, Memory: 51848 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+from collections import deque
+
+dq = deque()
+for i in range(1, int(input())+1):
+    dq.append(i)
+
+while len(dq) > 1:
+    dq.popleft()
+    dq.append(dq.popleft())
+
+print(dq.pop())
