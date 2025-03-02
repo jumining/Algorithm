2a50261fa243143f23156f54372b0ad744fa413c
jumining <76610340+jumining@users.noreply.github.com>
2025-03-02 15:33:21 +0900

    [Gold V] Title: 회의실 배정, Time: 288 ms, Memory: 55000 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.py
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.py
@@ -4,19 +4,12 @@ input = sys.stdin.readline
 dic = []
 for _ in range(int(input())):
     dic.append((tuple(map(int, input().split()))))
-dic = sorted(dic, key=lambda x : (x[1], x[0]))    
 
-ans = []
+dic = sorted(dic, key=lambda x : (x[1], x[0]))    
 cnt = 0
-cur_st, cur_en = 0, 0
+t = 0
 for st, en in dic:
-    if st == en:
-        cur_en = en
+    if st >= t:
+        t = en
         cnt += 1
-        ans.append((st,en))
-        continue
-    if st >= cur_en:
-        cur_st, cur_en = st, en
-        cnt += 1
-        ans.append((st,en))
-print(len(ans))
+print(cnt)
