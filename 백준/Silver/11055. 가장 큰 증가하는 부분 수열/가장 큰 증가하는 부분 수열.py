6b720fad0db19a3773b77faa096f3e77cd60e01e
jumining <76610340+jumining@users.noreply.github.com>
2025-04-04 15:53:15 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 140 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.py
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.py
@@ -6,9 +6,8 @@ arr = list(map(int, input().split()))
 dp = arr[:]
 
 for i in range(num):
-    for j in range(i-1, -1, -1):
+    for j in range(i):
         if arr[j] < arr[i]:
-            # print(f"    {i}: {dp[i]}, {arr[i]+dp[j]}")
-            dp[i] = max(dp[i], arr[i]+dp[j])
+            dp[i] = max(dp[i], arr[i] + dp[j])
             
 print(max(dp))         
