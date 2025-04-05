2010d58b463000d0cffa52a49910b12cd0fcffb5
jumining <76610340+jumining@users.noreply.github.com>
2025-04-05 12:50:31 +0900

    [Gold IV] Title: 뱀, Time: 60 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.py
+++ 백준/Gold/3190. 뱀/뱀.py
@@ -1,92 +1,53 @@
 from collections import deque
 
-# 방향 정보 변환
-def rotate(dy, dx, cmd):
-    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-    idx = directions.index((dy, dx))
-    if cmd == 'D':  # 오른쪽 회전
-        idx = (idx + 1) % 4
-    elif cmd == 'L':  # 왼쪽 회전
-        idx = (idx - 1) % 4
-    
-    # print(f"[방향 전환] '{cmd}' 입력 → 새로운 방향: {directions[idx]}")
-    return directions[idx]
-
-# 디버깅용 출력 함수
-def printArr(title, arr):
-    print(f"--- {title} ---")
-    for row in arr:
-        print(*row)
-    print()
-
+# 입력
 N = int(input())
 K = int(input())
-snake = [[0] * N for _ in range(N)]
 
-# 사과 위치정보 배열 입력
-apples = [[0] * N for _ in range(N)]
+# 입력 - 사과 
+apples = set()
 for _ in range(K):
     y, x = map(int, input().split())
-    apples[y-1][x-1] = 1
+    apples.add((y-1, x-1))
 
-# 행동 정보 배열에 저장
+# 입력 - 행동
 L = int(input())
-acts = []
+acts = {}
 for _ in range(L):
-    sec, dir = map(str, input().split())
-    acts.append((int(sec), dir))
+    sec, dir = input().split()
+    acts[int(sec)] = dir
+
+snake = deque([(0,0)]) # 꼬리 제거하는 용도
 
 def solution():
+    dir = [(0, 1), (1, 0), (0, -1), (-1, 0)]
     # 사전 뱀 시작 정보
-    hy, hx = 0, 0
-    dy, dx = 0, 1
-    snake[hy][hx] = 1
-    cnt = 0
-    length = 1
-    q = deque()
-    q.append((hy, hx))
+    dir_idx = 0
+    time = 0
+    nhy, nhx = 0, 0
 
-    acts.append((10001, ''))
+    while True:
+        time += 1
+        dy, dx = dir[dir_idx]
+        nhy = nhy + dy
+        nhx = nhx + dx
 
-    for sec, next_dir in acts:
-        while True:
-            if cnt == sec:
-                dy, dx = rotate(dy, dx, next_dir)
-                # print(f"[{cnt}초] 방향 전환 완료 → 다음 이동\n")
-                break    
-            cnt += 1
-            hy += dy
-            hx += dx
+        # 몸 또는 벽 충돌
+        if nhy < 0 or nhy >= N or nhx < 0 or nhx >= N or (nhy, nhx) in snake:
+            return time
 
-            # print(f"[{cnt}초] 뱀 머리 이동: ({hy}, {hx})")
+        # 한칸 움직이기
+        snake.append((nhy, nhx))
 
-            # 벽에 부딪힘
-            if hy < 0 or hy >= N or hx < 0 or hx >= N:
-                # print("💥 벽에 충돌!")
-                return cnt
-            
-            # 자기 몸에 부딪힘
-            if snake[hy][hx] == 1:
-                # print("💥 자기 몸에 충돌!")
-                return cnt
+        if (nhy, nhx) in apples:  # 사과 있음
+            apples.remove((nhy, nhx))
+        else:  # 사과 없음 → 꼬리 제거
+            snake.popleft()
 
-            # 한칸 움직이기
-            snake[hy][hx] = 1
-            q.append((hy, hx))
+        if time in acts:
+            if acts[time] == 'D':
+                dir_idx = (dir_idx + 1) % 4
+            elif acts[time] == 'L':
+                dir_idx = (dir_idx - 1) % 4
 
-            if apples[hy][hx] == 1:  # 사과 있음
-                apples[hy][hx] = 0
-                length += 1
-                # print("🍎 사과 먹음! 길이 증가")
-            else:  # 사과 없음 → 꼬리 제거
-                ty, tx = q.popleft()
-                snake[ty][tx] = 0
-                # print(f"🐍 꼬리 제거 at ({ty}, {tx})")
-
-            # printArr(f"시간 {cnt} 뱀 상태", snake)
-
-        
-
-# 프린트
-# printArr('초기 사과 위치', apples)
 print(solution())
