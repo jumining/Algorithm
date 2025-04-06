c667189298f5c471deaaa343f075c614d60b1bd4
jumining <76610340+jumining@users.noreply.github.com>
2025-04-06 17:04:29 +0900

    [Gold IV] Title: 마법사 상어와 파이어볼, Time: 596 ms, Memory: 36504 KB -BaekjoonHub

--- 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.py
+++ 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.py
@@ -5,46 +5,49 @@ for _ in range(M):
     y, x, m, s, d = map(int, input().split())
     balls.append((y-1,x-1,m,s,d))
 
-# 8가지 방향
-dir = [(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1)]
+# 방향 (상수)
+DIRECTIONS = [(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1)]
 
 # 짝홀 판단 함수
 def is_all_even_or_odd(lst):
     return all(l%2==0 for l in lst) or all(l%2==1 for l in lst)
 
-# 각 볼 방향대로 속력만큼 이동
+# 파이어볼 이동
 def move_balls(balls):
     board = [[[] for _ in range(N)] for _ in range(N)]
     for y,x,m,s,d in balls:
-        dy, dx = dir[d]
+        dy, dx = DIRECTIONS[d]
         ny, nx = (y+dy*s)%N, (x+dx*s)%N
         board[ny][nx].append((m,s,d))        
     return board
 
+# 파이어볼 분할
 def split_balls(board):
     balls = []
     for y in range(N):
         for x in range(N):
-            cnt_ball = len(board[y][x])
-            if cnt_ball >= 2:
-                new_s = sum(s for _,s,_ in board[y][x]) // cnt_ball
-                new_m = sum(m for m,_,_ in board[y][x]) // 5
-                if new_m==0:
-                    continue
+            cell = board[y][x]
+            cnt = len(cell)
+            
+            if cnt == 0:
+                continue
+            if cnt == 1:
+                balls.append((y,x,*cell[0]))
+                continue
 
-                arr_d = [d for _,_,d in board[y][x]]
-                new_dirs = [0,2,4,6] if is_all_even_or_odd(arr_d) else [1,3,5,7]
-                
-                for idx in new_dirs:
-                    dy, dx = dir[idx]
-                    balls.append((y,x,new_m,new_s,idx))
+            new_m = sum(m for m,_,_ in cell) // 5
+            if new_m==0:
+                continue
 
-            elif cnt_ball == 1:
-                m,s,d=board[y][x][0]
-                balls.append((y,x,m,s,d))
+            new_s = sum(s for _,s,_ in cell) // cnt
+            directions = [d % 2 for _, _, d in cell]
+            new_dirs = [0, 2, 4, 6] if all(d == directions[0] for d in directions) else [1, 3, 5, 7]
+            for d in new_dirs:
+                balls.append((y,x,new_m,new_s,d))
+
     return balls        
 
-# K번 파이어볼 이동
+# 시뮬레이션
 for step in range(K):
     board = move_balls(balls)
     balls = split_balls(board)
