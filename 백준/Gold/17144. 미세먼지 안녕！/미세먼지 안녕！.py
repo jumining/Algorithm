1a0be23710a354ac37457a33a728696d8c1b5580
jumining <76610340+jumining@users.noreply.github.com>
2025-04-07 20:22:57 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 2116 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
@@ -3,34 +3,29 @@
 DIR = [(-1,0),(0,1),(1,0),(0,-1)]
 
 def dust_spread():
-    global R, C
+    # 더해질 값 보관
+    tmp = [[0] * C for _ in range(R)]
 
-    tmp = [[[0, 0] for _ in range(C)] for _ in range(R)]
     for y in range(R):
         for x in range(C):
-            cnt = 0
-            reassign = []
-            if arr[y][x] != 0 and arr[y][x] != -1:
+            if arr[y][x] > 0:
+                cnt = 0
+                spread_amount = arr[y][x] // 5
                 for dy, dx in DIR:
                     ny, nx = y+dy, x+dx
                     if 0<=ny<R and 0<=nx<C and arr[ny][nx] != -1:
                         cnt += 1
-                        reassign.append((ny,nx))
-                
-                new_val = arr[y][x] // 5
-                for ry, rx in reassign:
-                    tmp[ry][rx][1] += new_val
-                remain_val = arr[y][x] - new_val * cnt
-                tmp[y][x][0] = remain_val
+                        tmp[ny][nx] += spread_amount
+                arr[y][x] -= cnt * spread_amount
     
     # arr에 적용
     for y in range(R):
         for x in range(C):
-            if tmp[y][x] != [0,0]:
-                arr[y][x] = tmp[y][x][0] + tmp[y][x][1]
+            if arr[y][x] != -1:
+                arr[y][x] += tmp[y][x]
 
-def air_clean(a, b):
-    global R, C
+def air_clean():
+    a, b = air_cleaner
 
     # 반시계 순환 
     for i in range(a-1, 0, -1): 
@@ -57,17 +52,17 @@ def air_clean(a, b):
 
 # 입력
 R, C, T = map(int, input().split())
-
 arr = []
-a, b = -1, -1
+air_cleaner = []
+
 for y in range(R):
     lst = list(map(int, input().split()))
     arr.append(lst)
-    if lst[0] == -1 and a == -1:
-        a, b = y, y+1
+    if lst[0] == -1:
+        air_cleaner.append(y)
 
 for i in range(T):
     dust_spread()
-    air_clean(a, b)
+    air_clean()
 
-print(sum(x for row in arr for x in row if x != -1))
+print(sum(sum(row) for row in arr) + 2)
