cf26d2904c1e2d239f16a6cf7e9c7b0ccf079122
jumining <76610340+jumining@users.noreply.github.com>
2025-04-09 14:16:15 +0900

    [Gold V] Title: 마법사 상어와 비바라기, Time: 196 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
+++ 백준/Gold/21610. 마법사 상어와 비바라기/마법사 상어와 비바라기.py
@@ -1,9 +1,8 @@
-# 11:53 - 12:00, 13:13-
+# 1h 10m 
 
 DIAGONAL = [(-1,-1), (-1,1), (1,1), (1,-1)]
 DIR = [(),(0,-1),(-1,-1),(-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1)]
 
-# 모든 구름 이동
 def move(d,s):
     dy, dx = DIR[d]
     new_clouds = []
@@ -13,19 +12,21 @@ def move(d,s):
     return new_clouds
 
 def watercopy(y, x):
-    # 대각선 방향으로 거리가 1인 칸에 물이 있는 바구니의 수만큼 
-    # (r,c)에 있는 바구니의 물 양 증가
-    # 행, 열 연결 가정 X
     cnt = 0
-    tmp = []
     for dy, dx in DIAGONAL:
         ny, nx = y+dy, x+dx
         if 0<=ny<N and 0<=nx<N and board[ny][nx] != 0:
             cnt += 1
-            tmp.append((ny,nx))
-
-
     board[y][x] += cnt
+
+def make_new_clouds():
+    new_clouds = []
+    for y in range(N):
+        for x in range(N):
+            if board[y][x] >= 2 and not isUsed[y][x]:
+                board[y][x] -= 2
+                new_clouds.append((y,x))
+    return new_clouds
 
 # 입력
 N, M = map(int, input().split())
@@ -38,7 +39,7 @@ for step, (d, s) in enumerate(cmd,1):
     increased = []
     isUsed = [[False] * N for _ in range(N)]
 
-    # 모든 구름이 d방향으로 s칸 이동
+    # 모든 구름 이동
     clouds = move(d,s)
 
     # 모든 구름 있는 칸의 물의 양 += 1
@@ -54,12 +55,8 @@ for step, (d, s) in enumerate(cmd,1):
     for y,x in increased:
         watercopy(y,x)
     
-    # 물의 양 2 이상이고 이전에 구름 안생겼으면 
-    # 물의 양 2 줄어들고 구름 생김
-    for y in range(N):
-        for x in range(N):
-            if board[y][x] >= 2 and not isUsed[y][x]:
-                board[y][x] -= 2
-                clouds.append((y,x))
+    # 구름 생김
+    clouds = make_new_clouds()
 
+# 출력
 print(sum(sum(row) for row in board))
