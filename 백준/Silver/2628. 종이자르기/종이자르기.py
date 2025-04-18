ade6477d5f2a0ce37e00e173d13f688e61c6ad99
jumining <76610340+jumining@users.noreply.github.com>
2025-04-18 23:12:00 +0900

    [Silver V] Title: 종이자르기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2628. 종이자르기/종이자르기.py
+++ 백준/Silver/2628. 종이자르기/종이자르기.py
@@ -3,23 +3,21 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())
 K = int(input())
-yarr = [0]
-xarr = [0]
+
+yarr = [0, M]
+xarr = [0, N]
+
 for _ in range(K):
     a, b = map(int, input().split())
     if a==0:
         yarr.append(b)
     else:
         xarr.append(b)
-yarr.sort()
-yarr.append(M)
-xarr.sort()
-xarr.append(N)
 
-ygap = []
-xgap = []
-for i in range(len(yarr)-1):
-    ygap.append(yarr[i+1]-yarr[i])
-for i in range(len(xarr)-1):
-    xgap.append(xarr[i+1]-xarr[i])
-print(max(ygap) * max(xgap))
+yarr.sort()
+xarr.sort()
+
+max_y_gap = max(yarr[i+1] - yarr[i] for i in range(len(yarr)-1))
+max_x_gap = max(xarr[i+1] - xarr[i] for i in range(len(xarr)-1))
+
+print(max_y_gap * max_x_gap)
