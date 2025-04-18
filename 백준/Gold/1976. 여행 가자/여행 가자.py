4c3e345d5275d48768dae7fed903f8e1e4448041
jumining <76610340+jumining@users.noreply.github.com>
2025-04-19 00:42:31 +0900

    [Gold IV] Title: 여행 가자, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -1,32 +1,34 @@
-from collections import deque
 import sys
 input = sys.stdin.readline
 
 N = int(input())
 M = int(input())
-city = [list(map(int, input().split())) for _ in range(N)]
+
+# x가 속한 집합의 대표를 찾는 함수
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x]) # 경로 압축
+    return parent[x]
+
+# x와 y가 속한 집합을 하나로 합침
+def union(x, y):
+    x_root = find(x)
+    y_root = find(y)
+    if x_root != y_root:
+        parent[y_root] = x_root
+
+# x의 대표(부모 노드) 저장
+parent = [i for i in range(N)]
+
+for i in range(N):
+    row = list(map(int, input().split()))
+    for j in range(N):
+        if row[j]:
+            union(i, j)
+
 plan = [x-1 for x in map(int, input().split())]
 
-def bfs(start, end):
-    visited = [False] * N
-    q = deque([start])
-    visited[start] = True
+first_root = find(plan[0])
+flag = all(find(city) == first_root for city in plan)
 
-    while q:
-        cur = q.popleft()
-        if cur == end:
-            return True
-
-        for i in range(N):
-            if not visited[i] and city[cur][i]:
-                visited[i] = True
-                q.append(i)
-
-    return False
-
-flag = True
-for i in range(len(plan)-1):
-    if not bfs(plan[i], plan[i+1]):
-        flag = False
-
-print("YES") if flag else print("NO")
+print("YES" if flag else "NO")
