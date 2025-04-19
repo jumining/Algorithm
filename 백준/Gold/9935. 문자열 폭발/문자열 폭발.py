5c0e259e3aecb9f0fd0559a66066858344042907
jumining <76610340+jumining@users.noreply.github.com>
2025-04-19 22:28:51 +0900

    [Gold IV] Title: 문자열 폭발, Time: 356 ms, Memory: 43596 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -7,8 +7,7 @@ bomb = input().strip()
 stack = []
 for c in target:
     stack.append(c)
-    if ''.join(stack[-len(bomb):]) == bomb:
-        for _ in range(len(bomb)):
-            stack.pop()
+    if c == bomb[-1] and ''.join(stack[-len(bomb):]) == bomb:
+        del stack[-len(bomb):]
 
 print(''.join(stack)) if stack else print("FRULA")
